6e32da5e544c465b1bdaa6059246f9742d34c3e7
Perminov A.O. <Triela1991@mail.ru>
2024-02-26 09:51:20 +0700

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,9 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
-import javax.swing.text.Document;
-import javax.swing.text.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -23,6 +19,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -30,12 +29,16 @@ public class Main {
 
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
+        String fileNameCSV = "data.json";
+        String fileNameXML = "data2.json";
 
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
-        writeString(json);
+        writeString(json, fileNameCSV);
 
         List<Employee> list_XML = parseXML("data.xml");
+        String jsonXML = listToJson(list_XML);
+        writeString(jsonXML, fileNameXML);
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -61,12 +64,11 @@ public class Main {
         builder.setPrettyPrinting();
         Gson gson = builder.create();
         String json = gson.toJson(list, listType);
-        //System.out.println(json);
         return json;
     }
 
-    public static void writeString(String list) {
-        try (FileWriter file = new FileWriter("data.json")) {
+    public static void writeString(String list, String name) {
+        try (FileWriter file = new FileWriter(name)) {
             file.write(list.toString());
             file.flush();
         } catch (IOException ex) {
@@ -77,25 +79,31 @@ public class Main {
     public static List<Employee> parseXML(String list) throws IOException, SAXException, ParserConfigurationException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = (Document) builder.parse(new File(list));
+        Document doc = builder.parse(new File(list));
         Node root = doc.getDocumentElement();
-        //System.out.println(root.getNodeName());
         return read(root);
     }
 
     public static List<Employee> read(Node node) {
+        List<Employee> staff = new ArrayList<>();
+
         NodeList nodeList = node.getChildNodes();
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node_ = nodeList.item(i);
-            if (Node.ELEMENT_NODE == node_.getNodeType()) {
-                Element element = (Element) node_;
-                NamedNodeMap map = element.getAttributes();
-                for (int a = 0; a < map.getLength(); a++) {
 
-                }
-                read(node_);
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                Element employee = (Element) node_;
+
+                long id = Integer.parseInt(employee.getElementsByTagName("id").item(0).getTextContent());
+                String firstName = employee.getElementsByTagName("firstName").item(0).getTextContent();
+                String lastName = employee.getElementsByTagName("lastName").item(0).getTextContent();
+                String country = employee.getElementsByTagName("country").item(0).getTextContent();
+                int age = Integer.parseInt(employee.getElementsByTagName("age").item(0).getTextContent());
+
+                Employee employees = new Employee(id, firstName, lastName, country, age);
+                staff.add(employees);
             }
         }
-        return null;
+        return staff;
     }
 }
